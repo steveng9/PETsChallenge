4d02d43e32054dcb1515c430c92500d5a4c57631
Steven Golob <golobs@uw.edu>
2023-04-19 22:29:44 -0700

    remove reference to swift in format builder

--- data/fincrime/centralized/test/formatbuilder.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-swift_df = pd.read_csv("C:/Users/deek/Documents/School/PET Prize/pets-prize-challenge-runtime/data/fincrime/dev_swift_transaction_test_dataset.csv")
-
-format_df = pd.DataFrame()
-format_df['MessageId'] = swift_df['MessageId']
-format_df['Score'] = 0.5
-print(format_df)
-format_df.to_csv('predictions_format.csv', index=False)
